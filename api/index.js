b01a13c2c59727bde6347643c4035d21df175649
Umoh Samuel Godwin <youngking074@gmail.com>
2023-09-28 11:53:02 +0100

    create middle and functon to handle possible error

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB!");
-}).catch(err =>{
+  })
+  .catch((err) => {
     console.log(err);
-})
-
+  });
 
 const app = express();
 
@@ -23,3 +25,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
